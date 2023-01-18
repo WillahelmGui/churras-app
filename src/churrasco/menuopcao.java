8a0ff823e715b4702617c5dd1942e3697fb39ab8
gabriel henrique <gabriel.2019monster@gmail.com>
2023-01-18 19:29:16 -0300

    subida de cod parte 01

---
+++ src/churrasco/menuopcao.java
@@ -0,0 +1,52 @@
+package churrasco;
+
+import java.util.Scanner;
+
+public class menuopcao {
+
+	Scanner sc = new Scanner(System.in);
+
+	public static void menuopcao() {
+		
+		System.out.println("escolha uma das opcao");
+		System.out.println("01  calcular");
+		System.out.println("0  sair");
+		
+
+	}
+		
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		byte opcaoEscolhida = -1;
+		menuopcao();
+
+		while (opcaoEscolhida != 1) {
+
+			opcaoEscolhida = sc.nextByte();
+
+			switch (opcaoEscolhida) {
+			case 1:
+				calculo.calcular();
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+				
+				break;
+
+			default:
+				System.out.println("digite um numero valido" + opcaoEscolhida);
+				break;
+			}
+			sc.close();
+		}
+
+	}
+
+}
