654d526bd1b5c88fc5e43ff692070cd7baf4d2e7
Pedrolobatto <59872519+predojjp@users.noreply.github.com>
2023-01-23 10:33:22 -0300

    Update MenuOpcao.java

--- src/churrasco/MenuOpcao.java
+++ src/churrasco/MenuOpcao.java
@@ -31,6 +31,8 @@ public class MenuOpcao {
 
 			switch (opcaoEscolhida) {
 			case 1:
+			cadastro = new  churrasAppCadastro();
+			cadastro.CadastroClass();
 				break;
 			case 2:
 				SelecaoDeCarnes.selecao();
