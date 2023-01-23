d1bfdeba1ceb9c206ef98ac600fb3f494772a849
Pedrolobatto <59872519+predojjp@users.noreply.github.com>
2023-01-23 19:56:58 -0300

    Update Cadastro.java

--- src/churrasco/Cadastro.java
+++ src/churrasco/Cadastro.java
@@ -10,32 +10,31 @@ public class Cadastro {
 	private static Scanner scan;
 	
 	public static void CadastroClass() {
-		System.out.println("\\--------------------------------------------------\\");
-		System.out.println("              CADASTRO SELECIONADO");
-		System.out.println("\\--------------------------------------------------\\");
-
-		System.out.println(" ");
+		System.out.println("\n/--------------------------------------------------\\");
+		System.out.println(" 		CADASTRO SELECIONADO");
+		System.out.println("\\--------------------------------------------------/");
 		
 		ArrayList<String> nome = new ArrayList<>();
 		scan = new Scanner(System.in);
 		int quantosNomes;
-		System.out.println("quantos participantes comparecerão? ");
+		
+		System.out.println("quantas pessoas comparecerão? ");
 		quantosNomes = scan.nextInt();
 		
 		for(int i = 1; i<= quantosNomes; i++) {
-		System.out.println("digite o nome do participante: ");
+		System.out.println("digite o nome dos participantes: ");
 		nome.add(scan.next()+ scan.nextLine());
 		
 		}
-		System.out.println("\\--------------------------------------------------\\");
+		System.out.println("\n/--------------------------------------------------\\");
 		System.out.println("       LISTA DE PARTICIPANTES QUE COMPARECERÂO");
-		System.out.println("\\--------------------------------------------------\\");
-		System.out.println(" ");
+		System.out.println("\\--------------------------------------------------/");
 
 		for(int j = 0; j < nome.size(); j++) {
-			System.out.println(nome.get(j));
+			
+			System.out.println(j + 1 +". " + nome.get(j));
 		}
-
+		
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println(" ");
