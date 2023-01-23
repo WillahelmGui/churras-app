ed821fc02ac1ffdebc2e3671d17957b0d963a0b4
Pedrolobatto <59872519+predojjp@users.noreply.github.com>
2023-01-23 18:06:26 -0300

    Update Cadastro.java

--- src/churrasco/Cadastro.java
+++ src/churrasco/Cadastro.java
@@ -1,40 +1,35 @@
 package churrasco;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cadastro {
-	static Scanner scan;
+	
+	
 
-
+	private static Scanner scan;
+	
 	public static void CadastroClass() {
 		
 		System.out.println("      CADASTRO SELECIONADO");
 		System.out.println(" ");
-
-		scan = new Scanner(System.in);
-		int quantosNomes;
-
 		
 		ArrayList<String> nome = new ArrayList<>();
+		scan = new Scanner(System.in);
+		int quantosNomes;
 		System.out.println("quantos participantes comparecerão? ");
 		quantosNomes = scan.nextInt();
 		
-		
-		for(int i = 1; i<=quantosNomes; i++) {
+		for(int i = 1; i<= quantosNomes; i++) {
 		System.out.println("digite o nome do participante: ");
-		
-		nome.add(scan.next());
-	
+		nome.add(scan.next()+ scan.nextLine());
+
 		}
+
 		System.out.println("os paritipantes: "+ nome + " \nconfirmaram a presença");
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println(" ");
-
-	
 		
 	}
-	
 }
