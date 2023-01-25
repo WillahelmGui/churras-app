f70a83e04b72b945b321b233fd2df58ef4f1af79
Pedro Lobato <59872519+predojjp@users.noreply.github.com>
2023-01-25 18:11:04 -0300

    Update Calculokg.java

--- src/churrasco/Calculokg.java
+++ src/churrasco/Calculokg.java
@@ -22,8 +22,9 @@ public class Calculokg {
 		} else {
 			System.out.println(" ");
 			System.out.println("\n/--------------------------------------------------\\");
-			System.out.println("    CADA UM DOS " + Cadastro.quantosNomes+ " PARTICIPANTES DEVE COMPRAR: ");
+			System.out.println("    CADA UM DOS " + Cadastro.quantosNomes+ " PARTICIPANTES DEVEM COMPRAR: ");
 			System.out.println("\\--------------------------------------------------/");
+
 
 
 
@@ -33,13 +34,34 @@ public class Calculokg {
 						j + 1 + ". " + df.format((CarnePesoTotal / 1000) / SelecaoDeCarnes.produtosSelecionados.size())
 								+ "Kg de " + SelecaoDeCarnes.produtosSelecionados.get(j));
 				
-				
 			}
-			System.out.println("Kg de carnes: " + CarnePesoTotal / 1000 + " Kg.");
+			System.out.println("Kg total de carnes: " + CarnePesoTotal / 1000 + " Kg.");	
 			System.out.println("Preço total das carnes: "); //Não feito ainda
 		}
+	
+		System.out.println(" ");
+
 		
+		Bebidas.BebidasMLTotal = Cadastro.quantosNomes * Bebidas.BebidaPessoa;
+	if (Bebidas.BebidasMLTotal == 0) {
+		System.out.println("Epa! Você não cadastrou ninguém no seu churrasco! \n"
+				+ "Volte para o menu e cadastre os participantes. ");
+	} else {
+		
+		for (int k = 0; k < Bebidas.BebidasSelecionadas.size(); k++) {
+			System.out.println(
+					k + 1 + ". " + df.format((Bebidas.BebidasMLTotal / 1000) / Bebidas.BebidasSelecionadas.size())
+							+ "L de " + Bebidas.BebidasSelecionadas.get(k));
+		}
+		System.out.println("Litros total de Bebidas: " + Bebidas.BebidasMLTotal / 1000 + "L");
+		System.out.println("Preço total das bebidas: "); //Não feito ainda
+	
+		}
+	
+
+
 		System.out.println(" ");
 		System.out.println(" ");
-	}
+	
+}
 }
