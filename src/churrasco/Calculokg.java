ab9ce40dbe07c6aa43e3eacf10571add81fca4cb
Pedro Lobato <59872519+predojjp@users.noreply.github.com>
2023-01-25 16:17:42 -0300

    Update Calculokg.java

--- src/churrasco/Calculokg.java
+++ src/churrasco/Calculokg.java
@@ -14,22 +14,32 @@ public class Calculokg {
 		System.out.println(" 		CÁLCULO INICIALIZADO");
 		System.out.println("\\--------------------------------------------------/");
 
-		System.out.println(
-				"Considerando que cada pessoa bebe 1,4L e come aprox 550G no churrasco, \n" + "Você deve comprar:");
+		System.out.println(" Considerando que cada pessoa bebe 1,4L \n e come aproximadamente 550G no churrasco \n" );
 		CarnePesoTotal = Cadastro.quantosNomes * CarnePessoa;
 		if (CarnePesoTotal == 0) {
 			System.out.println("Epa! Você não cadastrou ninguém no seu churrasco! \n"
 					+ "Volte para o menu e cadastre os participantes. ");
 		} else {
-			System.out.println("Você deve comprar: " + CarnePesoTotal / 1000 + "Kg de carne, sendo eles: ");
+			System.out.println(" ");
+			System.out.println("\n/--------------------------------------------------\\");
+			System.out.println("    CADA UM DOS " + Cadastro.quantosNomes+ " PARTICIPANTES DEVE COMPRAR: ");
+			System.out.println("\\--------------------------------------------------/");
+
+
+
 			for (int j = 0; j < SelecaoDeCarnes.produtosSelecionados.size(); j++) {
 
 				System.out.println(
 						j + 1 + ". " + df.format((CarnePesoTotal / 1000) / SelecaoDeCarnes.produtosSelecionados.size())
 								+ "Kg de " + SelecaoDeCarnes.produtosSelecionados.get(j));
-				System.out.println("O preço das carnes será: ");
-				//Não feito ainda
+				
+				
 			}
+			System.out.println("Kg de carnes: " + CarnePesoTotal / 1000 + " Kg.");
+			System.out.println("Preço total das carnes: "); //Não feito ainda
 		}
+		
+		System.out.println(" ");
+		System.out.println(" ");
 	}
 }
